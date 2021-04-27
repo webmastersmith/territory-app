eaedd6117a4b7dbc68fc1276ac00df6c1fee2322
webmastersmith <bryon@smithauto.us>
2021-04-27 17:14:39 -0500

    connect to api

--- src/Controller.js
+++ src/Controller.js
@@ -3,21 +3,32 @@
 const MSG = {
 	ROAD: "ROAD",
 	ROAD_ID: "ROAD_ID",
+	KEY: "KEY",
 }
 
 export function getRoad(road) {return {type: MSG.ROAD,	road,}}
 export function fetchPropId(roadName) {return {type: MSG.ROAD_ID,	roadName,}}
+export function updateKey(key) {return {type: MSG.KEY,	key,}}
 
 function update(msg, model) {
 	switch (msg.type) {
 		case MSG.ROAD: {
 			const { road } = msg
-			console.log(road)
+			// console.log(road)
 			return { ...model, road }
+		}
+		case MSG.KEY: {
+			const { key } = msg
+			// console.log(road)
+			return { ...model, key }
 		}
 		case MSG.ROAD_ID: {
 			const { roadName } = msg
-			// console.log(propertySearch(roadName))
+			fetch('http://localhost:5500', {
+				method: 'POST',
+				
+			})
+			// console.log(roadName)
 			return { ...model }
 		}
 	}
