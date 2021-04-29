294b92e0be58c5fbc16ed2f4e8579f8bc0f6a874
webmastersmith <bryon@smithauto.us>
2021-04-28 23:23:18 -0500

    fix async call

--- src/Controller.js
+++ src/Controller.js
@@ -2,26 +2,36 @@ import * as R from 'ramda'
 
 const MSG = {
 	ROAD: "ROAD",
-	ROAD_URL: "ROAD_URL",
 	KEY: "KEY",
-	HTTP_SUCCESS: "HTTP_SUCCESS",
+	ROAD_LIST: "ROAD_LIST",
+	ROAD_ITEM: "ROAD_ITEM",
+	HTTP_SUCCESS_LIST: "HTTP_SUCCESS_LIST",
+	HTTP_SUCCESS_ITEM: "HTTP_SUCCESS_ITEM",
 	HTTP_ERROR: "HTTP_ERROR",
 	CLEAR_ERROR: "CLEAR_ERROR"
 }
 
-function returnUrl(key) {
+function roadListUrl(key) {
 	return `http://localhost:5500/${key}`
 }
-export function inputRoadName(road) {return {type: MSG.ROAD, road,}}
+function roadItemUrl(key) {
+	return `http://localhost:5500/lot/${key}`
+}
 // submit button on road name.
-export const getRoadUrl = { type: MSG.ROAD_URL }
-export function fetchPropId(roadName) {return {type: MSG.ROAD_ID, roadName,}}
+export const getRoadList = { type: MSG.ROAD_LIST }
+export const getRoadItem = { type: MSG.ROAD_ITEM }
+
+export function inputRoadName(road) {return {type: MSG.ROAD, road,}}
 export function updateKey(key) {return {type: MSG.KEY, key,}}
-const httpSuccessMsg = R.curry((roadName, response) => ({
-	type: MSG.HTTP_SUCCESS,
+const httpSuccessListMsg = R.curry((roadName, response) => ({
+	type: MSG.HTTP_SUCCESS_LIST,
 	roadName,
 	response
 }))
+const httpSuccessItemMsg = (response) => ({
+	type: MSG.HTTP_SUCCESS_ITEM,
+	response
+})
 const httpErrorMsg = (error) => ({ type: MSG.HTTP_ERROR, error})
 const clearError = { type: MSG.CLEAR_ERROR }
 
@@ -33,28 +43,45 @@ function update(msg, model) {
 		}
 		case MSG.KEY: {
 			const { key } = msg
-			// console.log(road)
 			return { ...model, key }
 		}
-		case MSG.ROAD_URL: {
-			// 1. build initial display
-			// 2. return roadName, 
+		case MSG.ROAD_LIST: {
 			return [
 				{...model, waiting: true},
 				{
 					request: { 
-						url: returnUrl(model.key),
+						url: roadListUrl(model.key),
 						data: { road: model.road },
 						method: 'post'
 					},
-					successMsg: httpSuccessMsg(model.road),
+					successMsg: httpSuccessListMsg(model.road),
 					errorMsg: httpErrorMsg(model.error)
 				}
 			]
 		}
-		case MSG.HTTP_SUCCESS: {
-			const { roadName, response } = msg
-			return {...model, waiting: false, roadIds: response.data}
+		case MSG.ROAD_ITEM: {
+			return [
+				{...model},
+				{
+					request: { 
+						url: roadItemUrl(model.key),
+						data: { roadIds: model.roadIds },
+						method: 'post'
+					},
+					successMsg: httpSuccessItemMsg,
+					errorMsg: httpErrorMsg(model.error)
+				}
+			]
+		}
+		case MSG.HTTP_SUCCESS_LIST: {
+			const { response } = msg
+			return { ...model, waiting: false, roadIds: response.data }
+		}
+		case MSG.HTTP_SUCCESS_ITEM: {
+			const { response } = msg
+			const owner = response.data
+			const owners = R.append(owner, model.owners)
+			return { ...model, waiting: false, owners }
 		}
 		case MSG.HTTP_ERROR: {
 			const { error } = msg
--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,29 +1,45 @@
 import hh from "hyperscript-helpers"
 import { h } from "virtual-dom"
-import { inputRoadName, getRoadUrl } from '../Controller'
+import { inputRoadName, getRoadList, getRoadItem } from '../Controller'
 import { btnCSS, btnDisabled } from './Button'
 
-const { div, label, input, form, button} = hh(h)
+const { div, label, input, form, button, p, span} = hh(h)
+
+function showLots(m) {
+	return span({className: `text-red-500`}, `${m.road} ${m.roadIds.length} LOTS`)
+}
 
 export default function InputBox(d, m) {
 	return div({ className: `my-10` }, [
 		form({className: ``,
 			onsubmit: e => {
 				e.preventDefault()
-				d(getRoadUrl)
+				m.roadIds.length ? d(getRoadItem) : d(getRoadList)  
 			}
 	}, [
 			label({className: `text-gray-600 pl-2`}, 'Road Name'),
-			input({
-				className: `border-black border block rounded-lg w-100 p-2 text-2xl`,
-				type: 'text',
-				value: m.road,
-				oninput: (e) => d(inputRoadName(e.target.value))
-			}),
-			button({
-				className: `${btnCSS('bg-blue-500', 'bg-blue-600', 'text-white', 'text-2xl')}`,
-				type:'submit'
-			}, 'find')			
-		])
-	])
+			div({className: `relative`}, [
+				input({
+					className: `
+					border-black border block rounded-lg w-100 p-2 text-2xl
+					${m.roadIds.length && 'text-green-500'}
+					`,
+					type: 'text',
+					value: m.roadIds.length 
+							? `${m.road}: \t ${m.roadIds.length} Lots `
+							: m.road,
+					oninput: (e) => d(inputRoadName(e.target.value))
+				}),
+				m.roadIds.length 
+					? button({
+					className: `${btnCSS('bg-green-500', 'bg-green-600', 'text-white', 'text-2xl')} absolute top-0 right-0 h-100`,
+					type:'submit'
+				}, 'find owners')
+					: button({
+					className: `${btnCSS('bg-blue-500', 'bg-blue-600', 'text-white', 'text-2xl')} absolute top-0 right-0 h-100`,
+					type:'submit'
+				}, 'find lots')
+			]), // end input box div
+		]) // end form
+	]) // end div
 }
