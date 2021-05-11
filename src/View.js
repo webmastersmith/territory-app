eb9a4f679b9f2b089c2101e335acf821198c5723
webmastersmith <bryon@smithauto.us>
2021-05-11 10:15:06 -0500

    fix background

--- src/View.js
+++ src/View.js
@@ -46,46 +46,44 @@ function getNumSize(num) {
 
 // total page view
 function view(dispatch, model) {
-	return div({className: ` h-100% bg-gray-100`}, [
-		div({ className: `h-100% sm:container mx-auto` }, [
-			// modal
-			div({className: `bg-gray-300 justify-center items-center w-screen min-h-screen z-10 absolute left-0 top-0 rounded-xl opacity-60 ${model.waiting ? 'flex' : 'hidden'}`}, [
-				spinner('blue'),
-			]),
-	
-			// top row of buttons
-			Buttons(dispatch, model),
-	
-			// Angelina County Territory
-			div({className: `sm:flex border-b-2 border-black`}, [
-				h1({ className: `text-center sm: text-left text-normal sm:text-2xl md:text-3xl font-bold` },
-					'Territory Address Finder'),
-					// key input box
-				input({
-					className: `w-100 mt-2 sm:mt-auto sm:w-48 md:w-52 border-2 border-green-500 mb-1 ml-auto rounded-md px-2 border-collapse `,
-					type: 'text',
-					value: model.key,
-					oninput: (e) => dispatch(updateKey(e.target.value))
-				},)
-			]),
-			InputBox(dispatch, model),
-			// Territory ID Message
-			div({className: `font-bold ${model.bulkUpload ? 'my-4 flex justify-center' : 'hidden'}`}, [
-				p({className: `text-3xl text-blue-700`}, [
-					`Territory `,
-					span({className: `relative`},[
-						`${model.territory}`,
-						span({className: `bg-red-500 flex justify-center items-center rounded-full text-white text-sm absolute ${getNumSize(model.owners.length)}`}, `${model.owners.length}`),
-					]),
-				]),
-			]),
-			error(dispatch, model),
-			// cards -check if empty
-			div({className: `grid grid-cols-auto-430 gap-6 justify-items-center`}, [
-					getCards(dispatch, model),
-				]),
-				pre(JSON.stringify(model, null, 2))
+	return div({ className: `h-100% sm:container mx-auto` }, [
+		// modal
+		div({className: `bg-gray-300 justify-center items-center w-screen min-h-screen z-10 absolute left-0 top-0 rounded-xl opacity-60 ${model.waiting ? 'flex' : 'hidden'}`}, [
+			spinner('blue'),
 		]),
+
+		// top row of buttons
+		Buttons(dispatch, model),
+
+		// Angelina County Territory
+		div({className: `sm:flex border-b-2 border-black`}, [
+			h1({ className: `text-center sm: text-left text-normal sm:text-2xl md:text-3xl font-bold` },
+				'Territory Address Finder'),
+				// key input box
+			input({
+				className: `w-100 mt-2 sm:mt-auto sm:w-48 md:w-52 border-2 border-green-500 mb-1 ml-auto rounded-md px-2 border-collapse `,
+				type: 'text',
+				value: model.key,
+				oninput: (e) => dispatch(updateKey(e.target.value))
+			},)
+		]),
+		InputBox(dispatch, model),
+		// Territory ID Message
+		div({className: `font-bold ${model.bulkUpload ? 'my-4 flex justify-center' : 'hidden'}`}, [
+			p({className: `text-3xl text-blue-700`}, [
+				`Territory `,
+				span({className: `relative`},[
+					`${model.territory}`,
+					span({className: `bg-red-500 flex justify-center items-center rounded-full text-white text-sm absolute ${getNumSize(model.owners.length)}`}, `${model.owners.length}`),
+				]),
+			]),
+		]),
+		error(dispatch, model),
+		// cards -check if empty
+		div({className: `grid grid-cols-auto-430 gap-6 justify-items-center`}, [
+				getCards(dispatch, model),
+			]),
+			pre(JSON.stringify(model, null, 2))
 	])
 }
 
